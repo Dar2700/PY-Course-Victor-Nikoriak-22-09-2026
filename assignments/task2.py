03d94208a2dbe92553baf3f133c499e51197f292
Daria Pidberezna <darinka9816@gmail.com>
2026-09-23 15:01:32 +0300

    Homework 01/Task2

---
+++ assignments/task2.py
@@ -0,0 +1,8 @@
+# one two three
+print("one", "two", "three")
+# one, two, three
+print("one", "two", "three", sep=", ")
+# Hello...
+print("Hello", end="...")
+# one, two...
+print("one", "two", sep=", ", end="...")
