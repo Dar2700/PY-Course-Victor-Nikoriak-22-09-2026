7733f3bfc3ea0deb9996d10243b6dc233b408a6a
Daria Pidberezna <darinka9816@gmail.com>
2026-09-25 22:17:50 +0300

    complete task4

---
+++ assignments/task4.py
@@ -0,0 +1,10 @@
+a=11
+b=15
+print(f"Addition: {a+b}")
+print(f"Subtraction: {a-b}")
+print(f"Multiplication: {a*b}")
+print(f"Division: {a/b:.2f}")
+print(f"Floor division: {a//b}")
+print(f"Modulus: {a%b}")
+print(f"Exponent: {a**b}")
+
